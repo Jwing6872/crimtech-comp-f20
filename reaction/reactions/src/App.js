ede11edb46f61da9412a611f9493c03db9dfc546
Jwing6872 <johnwang58@gmail.com>
2020-11-14 10:47:24 -0600

    finished

---
+++ reaction/reactions/src/App.js
@@ -0,0 +1,82 @@
+import logo from './logo.svg';
+import './App.css';
+import React from 'react';
+
+class Panel extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { 
+      start_time: 0, 
+      ran_once: false, 
+      counting: false, 
+      true_duration: 0, 
+      reaction_time: 0, 
+      color: 'green'};
+    this.process_click = this.process_click.bind(this);
+  }
+  handle_color = (c) => {
+    setTimeout(() => {
+      this.setState({color: c})
+    }, this.state.true_duration)
+  }
+  start_count() {
+    this.setState({
+      start_time: window.performance.now(),
+      true_duration: Math.random() * (7000 - 2000) + 2000,
+      counting: true,
+      color: "red"
+    })
+  }
+  end_count() {
+    if ((window.performance.now()-this.state.start_time) >= this.state.true_duration) {
+      this.setState((prev) => ({
+        ran_once: true,
+        counting: false,
+        reaction_time: window.performance.now() - (prev.start_time + prev.true_duration)
+      }))
+    }
+  }
+  process_click() {
+    if (this.state.counting) {
+      this.end_count();
+    } else {
+      this.start_count()
+      this.handle_color("green");
+    }
+  }
+  render() {
+    let msg;
+    if (this.state.counting) {
+      if (this.state.color == "green") {
+        msg = "Click!"
+      } else {
+        msg = "Wait for Green"
+      }
+    } else {
+      if (this.state.ran_once == true) {
+        msg = "Your reaction time is " + this.state.reaction_time + " ms."
+      } else {
+        msg = "Click me to begin!"
+      }
+    }
+    return (
+      <div className = "PanelContainer" onClick = {this.process_click} style={ { background: this.state.color} }>
+        <div className = "Panel">{msg}</div>
+      </div>
+    );
+  }
+}
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1 className =  "Header">How Fast is your Reaction Time?</h1>
+        <Panel />
+        <p>Click as soon as the red box turns green. Click anywhere in the box to start.</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
